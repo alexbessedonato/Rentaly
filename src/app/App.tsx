b41b7b0f24398feb4bf957249ad8a5a8e924028d
alexbessedonato <alexbessedonato03@gmail.com>
2026-06-28 22:34:35 +0200

    added vercel analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { router } from '../routes/router'
+import { Analytics } from '@vercel/analytics/react'
+
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Analytics />
     </QueryClientProvider>
   )
 }
